9ff3dffc3c04c3cdcdcb0791ed243e2c48f59af2
Renat Mavlyutov <mailren82@gmail.com>
2021-03-23 11:56:47 +0500

    Delete key)

--- app/settings.py
+++ app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i8cnmx_qmwgy&^#nz*5%$q80jdv8t-c!boqkg_oz0g%cy)on@)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
